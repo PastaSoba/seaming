303bb2a590406b5255b8162422040f25f330502e
PastaSoba <sanukinomiyatuko09@gmail.com>
2020-03-10 15:17:15 +0900

    ノードのモデルを作成した

--- src/node/admin.py
+++ src/node/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UnChangeableInfo, ChangeableInfo
 
-# Register your models here.
+
+class UnChangeableInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'url', 'position_x', 'position_y', 'parent_node')
+
+
+class ChangeableInfoAdmin(admin.ModelAdmin):
+    list_display = ('node', 'user', 'proficiency')
+
+
+admin.site.register(UnChangeableInfo, UnChangeableInfoAdmin)
+admin.site.register(ChangeableInfo, ChangeableInfoAdmin)
